be273638b2c518709fc5bf2c76c39b1402b6ad44
Maxime Tricoire <max.tricoire@gmail.com>
2016-08-25 14:34:26 +0200

    v0.1.2: new Error type (ModelValidationError) + minor fixes

--- index.js
+++ index.js
@@ -1,66 +1,76 @@
 'use strict';
 
 var kevoree = require('kevoree-library');
+var ModelValidationError = require('./lib/ModelValidationError');
 
 function testAttribute(instance, dictionary, attrType) {
   if (dictionary) {
-    if (dictionary.generated_KMF_ID === 0) {
+    if (dictionary.generated_KMF_ID === '0') {
       var attr = dictionary.findValuesByID(attrType.name);
       if (attr) {
         if (!attrType.optional) {
           if (!attr.value || attr.value.length === 0) {
             if (attrType.fragmentDependant) {
-              throw new Error('Missing value for non-optional fragmented attribute "'+attrType.name+'" in fragment "'+dictionary.name+'" for instance "' + instance.path() + '"');
+              throw new ModelValidationError('Missing value for non-optional fragmented attribute "'+attrType.name+'" in fragment "'+dictionary.name+'" for instance "' + instance.path() + '"');
             } else {
-              throw new Error('Missing value for non-optional attribute "'+attrType.name+'" for instance "' + instance.path() + '"');
+              throw new ModelValidationError('Missing value for non-optional attribute "'+attrType.name+'" for instance "' + instance.path() + '"');
             }
           }
         }
       } else {
         if (attrType.fragmentDependant) {
-          throw new Error('Missing fragmented attribute "'+attrType.name+'" in fragment "'+dictionary.name+'" for instance "' + instance.path() + '"');
+          throw new ModelValidationError('Missing fragmented attribute "'+attrType.name+'" in fragment "'+dictionary.name+'" for instance "' + instance.path() + '"');
         } else {
-          throw new Error('Missing attribute "'+attrType.name+'" for instance "' + instance.path() + '"');
+          throw new ModelValidationError('Missing attribute "'+attrType.name+'" for instance "' + instance.path() + '"');
         }
       }
     } else {
-      throw new Error('Dictionary KMF_ID must be set to "0" to prevent diff errors in instance "' + instance.path() + '"');
+      throw new ModelValidationError('Dictionary KMF_ID must be set to "0" to prevent diff errors in instance "' + instance.path() + '"');
     }
   } else {
-    throw new Error('Missing dictionary for instance "' + instance.path() + '"');
+    throw new ModelValidationError('Missing dictionary for instance "' + instance.path() + '"');
   }
 }
 
 module.exports = function (model) {
   var visitor = new kevoree.org.kevoree.modeling.api.util.ModelVisitor();
-  visitor.visit = function (instance, refInParent/*, parent*/) {
-    switch (refInParent) {
-      case 'nodes':
-      case 'groups':
-      case 'components':
-      case 'hosts':
-      case 'hubs':
+  visitor.visit = function (element/*, refInParent, parent*/) {
+    switch (element.metaClassName()) {
+      case 'org.kevoree.ContainerNode':
+      case 'org.kevoree.Group':
+      case 'org.kevoree.Channel':
+      case 'org.kevoree.ComponentInstance':
         // instance
-        if (!instance.typeDefinition) {
-          throw new Error('No TypeDefinition defined for instance "' + instance.path() + '"');
+        if (!element.typeDefinition) {
+          throw new ModelValidationError('No TypeDefinition defined for instance "' + element.path() + '"');
         }
-        if (instance.typeDefinition.dictionaryType) {
-          instance.typeDefinition.dictionaryType.attributes.array.forEach(function (type) {
+        if (element.typeDefinition.dictionaryType) {
+          element.typeDefinition.dictionaryType.attributes.array.forEach(function (type) {
             if (type.fragmentDependant) {
-              instance.fragmentDictionary.array.forEach(function (fDic) {
-                testAttribute(instance, fDic, type);
+              element.fragmentDictionary.array.forEach(function (fDic) {
+                testAttribute(element, fDic, type);
               });
             } else {
-              testAttribute(instance, instance.dictionary, type);
+              testAttribute(element, element.dictionary, type);
             }
           });
         } else {
-          if (instance.dictionary) {
-            throw new Error('Instance "'+ instance.path() +'" has a dictionary but it should not.');
+          if (element.dictionary) {
+            throw new ModelValidationError('Instance "'+ element.path() +'" has a dictionary but it should not.');
           }
-          if (instance.fragmentDictionary.size() > 0) {
-            throw new Error('Instance "'+ instance.path() +'" has fragment dictionaries but it should not.');
+          if (element.fragmentDictionary.size() > 0) {
+            throw new ModelValidationError('Instance "'+ element.path() +'" has fragment dictionaries but it should not.');
           }
+        }
+        break;
+
+      case 'org.kevoree.NodeType':
+      case 'org.kevoree.GroupType':
+      case 'org.kevoree.ChannelType':
+      case 'org.kevoree.ComponentType':
+        // type definition
+        if (element.deployUnits.size() === 0) {
+          throw new ModelValidationError('No DeployUnits defined for type "'+ element.path() +'"');
         }
         break;
     }
