c81ba0c3808d0198ec953f112017d5203ecab02e
Maxime Tricoire <max.tricoire@gmail.com>
2017-01-06 17:36:18 +0100

    v0.1.16: add element path to ModelValidationError attributes

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ function testAttribute(instance, dictionary, attrType) {
       if (!attrType.optional) {
         if (!attr.value || attr.value.length === 0) {
           if (attrType.fragmentDependant) {
-            throw new ModelValidationError('Missing value for non-optional fragmented attribute "'+attrType.name+'" in fragment "'+dictionary.name+'" for instance "' + instance.path() + '"');
+            throw new ModelValidationError('Missing value for non-optional fragmented attribute "'+attrType.name+'" in fragment "'+dictionary.name+'" for instance "' + instance.path() + '"', instance.path());
           } else {
-            throw new ModelValidationError('Missing value for non-optional attribute "'+attrType.name+'" for instance "' + instance.path() + '"');
+            throw new ModelValidationError('Missing value for non-optional attribute "'+attrType.name+'" for instance "' + instance.path() + '"', instance.path());
           }
         }
       }
     } else {
       if (attrType.fragmentDependant) {
-        throw new ModelValidationError('Missing fragmented attribute "'+attrType.name+'" in fragment "'+dictionary.name+'" for instance "' + instance.path() + '"');
+        throw new ModelValidationError('Missing fragmented attribute "'+attrType.name+'" in fragment "'+dictionary.name+'" for instance "' + instance.path() + '"', instance.path());
       } else {
-        throw new ModelValidationError('Missing attribute "'+attrType.name+'" for instance "' + instance.path() + '"');
+        throw new ModelValidationError('Missing attribute "'+attrType.name+'" for instance "' + instance.path() + '"', instance.path());
       }
     }
   } else {
-    throw new ModelValidationError('Missing dictionary for instance "' + instance.path() + '"');
+    throw new ModelValidationError('Missing dictionary for instance "' + instance.path() + '"', instance.path());
   }
 }
 
@@ -44,7 +44,7 @@ function testPlatforms(tdef) {
   platforms.forEach(function (platform) {
     var metas = tdef.select('deployUnits[]/filters[name=platform,value='+platform+']').array;
     if (metas.length !== 1) {
-      throw new ModelValidationError('TypeDefinition ' + tdef.name + '/' + tdef.version + ' has ' + metas.length + ' DeployUnits for the \''+platform+'\' platform (must be 1 per platform)');
+      throw new ModelValidationError('TypeDefinition ' + tdef.name + '/' + tdef.version + ' has ' + metas.length + ' DeployUnits for the \''+platform+'\' platform (must be 1 per platform)', tdef.path());
     }
   });
 }
@@ -57,7 +57,7 @@ module.exports = function (model) {
       case 'org.kevoree.DictionaryType':
         // dictionary
         if (element.generated_KMF_ID !== '0.0' && element.generated_KMF_ID !== 0.0) {
-          throw new ModelValidationError('Dictionary KMF_ID must be set to 0.0 to prevent diff errors in instance "' + element.eContainer().path() + '"');
+          throw new ModelValidationError('Dictionary KMF_ID must be set to 0.0 to prevent diff errors in instance "' + element.eContainer().path() + '"', element.eContainer().path());
         }
         break;
 
@@ -67,7 +67,7 @@ module.exports = function (model) {
       case 'org.kevoree.ComponentInstance':
         // instance
         if (!element.typeDefinition) {
-          throw new ModelValidationError('No TypeDefinition defined for instance "' + element.path() + '"');
+          throw new ModelValidationError('No TypeDefinition defined for instance "' + element.path() + '"', element.path());
         }
         if (element.typeDefinition.dictionaryType) {
           element.typeDefinition.dictionaryType.attributes.array.forEach(function (type) {
@@ -81,10 +81,10 @@ module.exports = function (model) {
           });
         } else {
           if (element.dictionary) {
-            throw new ModelValidationError('Instance "'+ element.path() +'" has a dictionary but it should not.');
+            throw new ModelValidationError('Instance "'+ element.path() +'" has a dictionary but it should not.', element.path());
           }
           if (element.fragmentDictionary.size() > 0) {
-            throw new ModelValidationError('Instance "'+ element.path() +'" has fragment dictionaries but it should not.');
+            throw new ModelValidationError('Instance "'+ element.path() +'" has fragment dictionaries but it should not.', element.path());
           }
         }
         break;
@@ -95,7 +95,7 @@ module.exports = function (model) {
       case 'org.kevoree.ComponentType':
         // type definition
         if (element.deployUnits.size() === 0) {
-          throw new ModelValidationError('No DeployUnits defined for type "'+ element.path() +'"');
+          throw new ModelValidationError('No DeployUnits defined for type "'+ element.path() +'"', element.path());
         }
         testPlatforms(element);
         break;
